26280ae3ed9684a35c38f747bbd3880acaf4f744
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-12-18 08:13:49 -0800

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -26,8 +26,8 @@ void selection_sort(int *array, size_t size)
 				temp = array[i];
 				array[i] = array[j];
 				array[j] = temp;
-				print_array(array, size);
 			}
 		}
+		print_array(array, size);
 	}
 }
