beef2bb406b4193a186294e6acc754cae579e57e
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-12-18 08:42:21 -0800

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,7 +11,7 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, idx;
 	int temp;
 
 	if (!array || size < 2)
@@ -19,15 +19,18 @@ void selection_sort(int *array, size_t size)
 
 	for (j = 0; j < (size - 1); j++)
 	{
+		idx = j;
 		for (i = j + 1; i < size; i++)
 		{
-			if (array[j] > array[i])
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
+			if (array[idx] > array[i])
+				idx = i;
 		}
-		print_array(array, size);
+		if (idx != j)
+		{
+			temp = array[idx];
+			array[idx] = array[j];
+			array[j] = temp;
+			print_array(array, size);
+		}
 	}
 }
