82963b60e0283843209fb6b36118eba3844a53f8
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-12-18 08:55:46 -0800

    Task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * using the Auick sort algorithm
+ * Prints the array after each time two elements are swaped
+ *
+ * @array: the array to sort
+ * @size: the size of the array
+ *
+ * Return: Nothing (void)
+ */
+void quick_sort(int *array, size_t size)
+{
+}
